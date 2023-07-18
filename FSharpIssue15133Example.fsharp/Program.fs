46f7b8cffa7d7874482ee467e4b3fe090e51f993
ijklam <ijklam@qq.com>
2023-07-18 22:19:12 +0800

    添加项目文件。

---
+++ FSharpIssue15133Example.fsharp/Program.fs
@@ -0,0 +1,59 @@
+﻿open Microsoft.EntityFrameworkCore
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open System.Linq
+
+[<CLIMutable>]
+type Blog =
+    {
+        BlogId: int
+        Url: string
+        Rating: int
+        Posts: List<Post>
+    }
+
+and [<CLIMutable>] Post =
+    {
+        PostId: int
+        Title: string
+        Content: string
+        BlogId: int
+        Blog: Blog
+    }
+
+type BloggingContext(options) =
+    inherit DbContext(options)
+
+
+    [<DefaultValue>] val mutable private _Blog : DbSet<Blog>
+    member this.Blogs
+        with get() = this._Blog
+        and set v = this._Blog <- v
+
+    [<DefaultValue>] val mutable private _Posts : DbSet<Post>
+    member this.Posts 
+        with get() = this._Posts
+        and set v = this._Posts <- v
+
+
+let builder = HostApplicationBuilder()
+builder.Services.AddSqlite<BloggingContext>("Data Source=./1.db") |> ignore
+builder.Logging.AddConsole() |> ignore
+
+let app = builder.Build()
+let db = app.Services.GetRequiredService<BloggingContext>()
+db.Database.EnsureCreated() |> ignore
+
+let ls = query {
+        for i in db.Blogs do
+            join j in db.Posts.Where(fun i -> i.Title.Length > 10) on (i.BlogId = j.BlogId)
+            select {|i=i;j=j|}
+}
+
+printfn "%s" (ls.ToQueryString())
+
+
+try
+    ls.Where(fun i -> i.i.BlogId = 1).First() |> ignore
+with _ -> ()
---
+++ FSharpIssue15133Example/Program.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+var builder = new HostApplicationBuilder();
+builder.Services.AddSqlite<BloggingContext>("Data Source=./1.db");
+builder.Logging.AddConsole();
+
+var app = builder.Build();
+
+var db = app.Services.GetRequiredService<BloggingContext>();
+db.Database.EnsureCreated();
+
+var ls = from i in db.Blogs
+         join j in db.Posts.Where(i =>i.Title.Length > 10) on i.BlogId equals j.BlogId
+         select new { i, j };
+Console.WriteLine(ls.ToQueryString());
+try
+{
+    ls.Where(i => i.i.BlogId == 1).First();
+}
+catch (Exception)
+{
+}
+
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+    public int Rating { get; set; }
+    public List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
+
+public class BloggingContext : DbContext
+{
+    public BloggingContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+}
